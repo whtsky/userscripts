b493fd89787d28f49f1ef7cd05e6c0e1a4df0350
Wu Haotian <whtsky@gmail.com>
2019-05-29 17:42:08 +0800

    Update is.uestc.attachment-name.user.js

--- is.uestc.attachment-name.user.js
+++ is.uestc.attachment-name.user.js
@@ -1,12 +1,12 @@
 // ==UserScript==
 // @name 电子科大信软学院 - 修正附件下载文件名
-// @version 2
+// @version 3
 // @match http://www.is.uestc.edu.cn/news.do
 // @match http://www.is.uestc.edu.cn/notice.do
 // @grant none
 // ==/UserScript==
 
-const attachmentRe = /附件\d+-(.+)$/
+const attachmentRe = /附件\d+[-：](.+)$/
 
 document.querySelectorAll('.text a').forEach((anchor) => {
   const match = anchor.innerText.match(attachmentRe)
@@ -14,3 +14,4 @@ document.querySelectorAll('.text a').forEach((anchor) => {
       anchor.download = match.pop().trim()
   }
 })
+
