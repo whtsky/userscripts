73ceb0de872b1b31bebaaab03c0d8b3a82474e4e
Wu Haotian <whtsky@gmail.com>
2019-05-30 23:41:50 +0800

    Create tjupt-direct_links.user.js

---
+++ tjupt-direct_links.user.js
@@ -0,0 +1,30 @@
+// ==UserScript==
+// @name TJUPT - Direct Links
+// @namespace https://github.com/whtsky/userscripts/
+// @version 20190530
+// @description 将 TJUPT 中的外链转换为直接链接
+// @match https://tjupt.org/*
+// @grant none
+// ==/UserScript==
+
+
+function removeTo(s, to) {
+  const index = s.search(to);
+  if (index !== -1) {
+    return s.substr(index + to.length);
+  }
+  return s;
+}
+
+
+document.querySelectorAll('a').forEach(anchor => {
+  let link = anchor.href;
+  if (link.startsWith("https://tjupt.org/adredir.php")) {
+    link = removeTo(link, "url=");
+  } else if (link.startsWith("https://tjupt.org/jump_external.php")) {
+    link = removeTo(link, "ext_url=");
+  } else {
+    return;
+  }
+  anchor.href = decodeURIComponent(link);
+})
