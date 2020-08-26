61e08eed77b94200ed8c60b5d26eaee73ee4d09d
Wu Haotian <whtsky@gmail.com>
2020-08-26 23:48:31 +0800

    fix(zhihu-daily-title): hijack document.title

--- zhihu-daily-title.user.js
+++ zhihu-daily-title.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name        知乎日报 - 更新 title
 // @match       https://daily.zhihu.com/*
-// @version      1.0
+// @version      1.1
 // @description  根据知乎日报的标题更新页面 title 标签
 // @downloadURL https://userscripts.whtsky.me/zhihu-daily-title.user.js
 // @updateURL https://userscripts.whtsky.me/zhihu-daily-title.user.js
@@ -11,15 +11,16 @@
 const questionTitle = document.querySelector('.DailyHeader-title').innerText
 const expectedTitle = `${questionTitle} - 知乎日报`
 
-function updateTitle() {
-  document.title = expectedTitle
-}
-
-new MutationObserver(function () {
-  // fight with react-helmet
-  if (document.title != expectedTitle) {
-    updateTitle()
+/**
+ * 知乎日报使用 React Helmet 更新 title 。
+ * React Helmet 使用 `document.title = xxx` 来更新 title ，所以无法使用 MutationObserver
+ * hijack document.title
+ */
+document.__defineSetter__('title', function (val) {
+  if (val != expectedTitle) {
+    return
   }
-}).observe(document.querySelector('title'), { subtree: true, characterData: true })
+  document.querySelector('title').childNodes[0].nodeValue = expectedTitle
+})
 
-updateTitle()
+document.title = expectedTitle
